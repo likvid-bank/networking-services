1d4b7994c7f4c8a2d981f183faa84195c1ffbb16
Terraform <unipipe-terraform-runner@meshcloud.io>
2022-11-02 13:28:44 +0000

    processed instances via unipipe terraform

--- instances/5a2a7ebf-b070-42c8-ae34-35e22e10f47e/bindings/5191217a-db4f-47a5-b3f4-398fa99cf329/backend.tf
+++ instances/5a2a7ebf-b070-42c8-ae34-35e22e10f47e/bindings/5191217a-db4f-47a5-b3f4-398fa99cf329/backend.tf
@@ -3,7 +3,7 @@ terraform {
     # tenant, subscription, client id and secret must be set by env variables ARM_* because they were not picked up here
  
     resource_group_name  = "unipipe-networking"
-    storage_account_name = "unipipenetworking"
+    storage_account_name = "unipipenetworkinglikvid"
     container_name       = "tfstates"
     key                  = "service-binding-"
   }
